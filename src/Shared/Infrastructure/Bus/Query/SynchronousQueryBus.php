cd5be5696684261a47d629d47c46a55d26ca8e09
Joel Coll Costa <joel.coll@gmail.com>
2021-02-19 15:22:29 +0100

    Create Symchronous implementation for QueryBus

---
+++ src/Shared/Infrastructure/Bus/Query/SynchronousQueryBus.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bray\Shared\Infrastructure\Bus\Query;
+
+use Bray\Shared\Domain\Bus\Query\Query;
+use Bray\Shared\Domain\Bus\Query\QueryBus;
+use Bray\Shared\Domain\Bus\Query\Response;
+use Symfony\Component\Messenger\MessageBus;
+
+final class SynchronousQueryBus implements QueryBus
+{
+    private MessageBus $bus;
+
+    public function __construct() {
+        $this->bus = new MessageBus();
+    }
+
+    public function ask(Query $query): ?Response {
+        return null;
+    }
+}
