3512d4cd0766775974ca19ad1729fd9e9c248c03
Joel Coll Costa <joel.coll@gmail.com>
2021-02-20 12:16:57 +0100

    Create bootstrap.php file

--- apps/socialnetwork/backend/public/index.php
+++ apps/socialnetwork/backend/public/index.php
@@ -1,18 +1,26 @@
 <?php
 
 use Bray\Apps\SocialNetwork\Backend\BraySocialNetworkBackendKernel;
-use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__) . '/../../../vendor/autoload.php';
-
-(new Dotenv())->bootEnv(dirname(__DIR__) . '/../../../.env');
+require dirname(__DIR__) . '/../../bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 
     Debug::enable();
+}
+
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(
+        explode(',', $trustedProxies),
+        Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST
+    );
+}
+
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$trustedHosts]);
 }
 
 $kernel = new BraySocialNetworkBackendKernel($_SERVER['APP_ENV'], (bool)$_SERVER['APP_DEBUG']);
