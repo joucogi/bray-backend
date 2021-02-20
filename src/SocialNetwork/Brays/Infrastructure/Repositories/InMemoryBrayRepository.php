f268489babce4ae6d6eb6334427c522203d152e8
Joel Coll Costa <joel.coll@gmail.com>
2021-02-20 19:40:36 +0100

    Fix namespaces and test case when there are no brays

--- src/SocialNetwork/Brays/Infrastructure/Repositories/InMemoryBrayRepository.php
+++ src/SocialNetwork/Brays/Infrastructure/Repositories/InMemoryBrayRepository.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Bray\SocialNetwork\Brays\Infrastructure\Repositories;
 
 use Bray\SocialNetwork\Brays\Domain\Contracts\BrayRepository;
-use Bray\SocialNetwork\Domain\Bray;
+use Bray\SocialNetwork\Brays\Domain\Bray;
 
 final class InMemoryBrayRepository implements BrayRepository
 {
