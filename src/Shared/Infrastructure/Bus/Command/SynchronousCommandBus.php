08121d9b98075b6025f7b19c02d9087fa01fc8b9
Joel Coll Costa <joel.coll@gmail.com>
2021-03-13 13:01:27 +0100

    Implement CommandBus

--- src/Shared/Infrastructure/Bus/Command/SynchronousCommandBus.php
+++ src/Shared/Infrastructure/Bus/Command/SynchronousCommandBus.php
@@ -2,25 +2,23 @@
 
 declare(strict_types=1);
 
-namespace Bray\Shared\Infrastructure\Bus\Query;
+namespace Bray\Shared\Infrastructure\Bus\Command;
 
-use Bray\Shared\Domain\Bus\Query\Query;
-use Bray\Shared\Domain\Bus\Query\QueryBus;
-use Bray\Shared\Domain\Bus\Query\Response;
+use Bray\Shared\Domain\Bus\Command\Command;
+use Bray\Shared\Domain\Bus\Command\CommandBus;
 use Bray\Shared\Infrastructure\Bus\CallableFirstParameterExtractor;
 use http\Exception\RuntimeException;
 use Symfony\Component\Messenger\Handler\HandlersLocator;
 use Symfony\Component\Messenger\MessageBus;
 use Symfony\Component\Messenger\Middleware\HandleMessageMiddleware;
-use Symfony\Component\Messenger\Stamp\HandledStamp;
 
-final class SynchronousQueryBus implements QueryBus
+final class SynchronousCommandBus implements CommandBus
 {
     private MessageBus $bus;
 
-    public function __construct(iterable $queryHandlers) {
+    public function __construct(iterable $commandHandlers) {
 
-        $handlersMapping = CallableFirstParameterExtractor::forCallables($queryHandlers);
+        $handlersMapping = CallableFirstParameterExtractor::forCallables($commandHandlers);
 
         $this->bus = new MessageBus([
             new HandleMessageMiddleware(
@@ -29,13 +27,13 @@ final class SynchronousQueryBus implements QueryBus
         ]);
     }
 
-    public function ask(Query $query): ?Response {
+    public function dispatch(Command $command): void {
         try {
-            $stamp = $this->bus->dispatch($query)->last(HandledStamp::class);
-
-            return $stamp->getResult();
+            $this->bus->dispatch($command);
         } catch (RuntimeException $e) {
             throw new RuntimeException($e->getMessage());
         }
     }
+
+
 }
