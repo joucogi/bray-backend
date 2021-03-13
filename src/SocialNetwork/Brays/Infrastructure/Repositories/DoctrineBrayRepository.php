e9924b866e0f1db26ec1bdabf091d948dec387b9
Joel Coll Costa <joel.coll@gmail.com>
2021-03-13 14:29:57 +0100

    Select DoctrineBrayRepository implementation for BrayRepository interface

---
+++ src/SocialNetwork/Brays/Infrastructure/Repositories/DoctrineBrayRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bray\SocialNetwork\Brays\Infrastructure\Repositories;
+
+use Bray\SocialNetwork\Brays\Domain\Contracts\BrayRepository;
+use Bray\SocialNetwork\Brays\Domain\Bray;
+
+final class DoctrineBrayRepository implements BrayRepository
+{
+    private $brays;
+
+    public function __construct() { $this->brays = []; }
+
+    public function searchAll(): array {
+        return $this->brays;
+    }
+
+    public function save(Bray $bray): void {
+        $this->brays[] = $bray;
+    }
+}
